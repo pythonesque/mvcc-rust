716011ac47f8a39f9ca0c79226a253b9a75b2194
Joshua Yanovski <pythonesque@gmail.com>
2014-12-03 03:21:58 -0800

    DSTing tuple headers.

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,36 @@
 #![feature(default_type_params)]
 #![feature(macro_rules)]
 
-/*use std::mem;
-use std::ptr::RawPtr;
-use std::raw::Repr;
-use std::raw::Slice as RawSlice;
-use std::uint;*/
-
 #[deriving(Show)]
-//#[repr(C)]
 pub struct Oid(u32);
 
 #[deriving(Show)]
-//#[repr(C)]
 pub struct TransactionId(u32);
 
 #[deriving(Show)]
-//#[repr(C)]
 pub struct CommandId(u32);
 
+pub type Datum = uint;
+
 #[deriving(Show)]
-//#[repr(C)]
 pub struct BlockNumber(u32);
 
 #[deriving(Show)]
-//#[repr(C)]
 pub struct BlockIdData {
     bi_hi: u16,
     bi_lo: u16,
 }
 
 #[deriving(Show)]
-//#[repr(C)]
 pub struct OffsetNumber(u16);
 
 #[deriving(Show)]
-//#[repr(C,packed)]
 pub struct ItemPointerData {
     ip_blkid: BlockIdData,
     ip_posid: OffsetNumber,
 }
 
 #[deriving(Show)]
-//#[repr(C,packed)]
 #[repr(packed)]
 pub struct HeapTupleFields {
     t_xmin: TransactionId, // inserting xact ID
@@ -51,60 +39,41 @@ pub struct HeapTupleFields {
 }
 
 #[deriving(Show)]
-//#[repr(C,packed)]
 pub struct NormalTupleHeaderData {
     t_heap: HeapTupleFields,
     t_ctid: ItemPointerData, // current t_ctid of this or newer tuple
 }
 
 #[deriving(Show)]
-//#[repr(C,packed)]
 pub struct MinimalTupleHeaderData; // unit struct
 
-#[deriving(Show)]
 #[repr(C)]
-pub struct HeapTupleHeaderData<T> {
-    t_data_: T,
+pub struct HeapTupleHeaderData<T, Sized? D> {
+    data_: T,
     // ^ - 18 bytes (normally) - ^
     t_infomask2: u16, // number of attributes + various flags
     t_infomask: u16, // various flag bits, see below
     t_hoff: u8, // sizeof header incl. bitmap, padding
     // ^ - 5 bytes, 23 bytes total (normally) - ^
-    t_bits_: [u8, .. 1] // bitmap of NULLs -- VARIABLE LENGTH
-    // MORE DATA FOLLOWS AT END OF STRUCT
+    bits_: [u8, .. 0], // bitmap of NULLs -- VARIABLE LENGTH
+    rest_: D, // More bits (if necessary) plus user data (suitably aligned)
 }
 
-impl<T> Deref<T> for HeapTupleHeaderData<T> {
+impl<T, Sized? D> Deref<T> for HeapTupleHeaderData<T, D> {
     #[inline]
-    fn deref(&self) -> &T { &self.t_data_ }
+    fn deref(&self) -> &T { &self.data_ }
 }
 
-impl<T> DerefMut<T> for HeapTupleHeaderData<T> {
+/*impl<T> DerefMut<T> for HeapTupleHeaderData<T> {
     #[inline]
     fn deref_mut(&mut self) -> &mut T { &mut self.t_data_ }
-}
-
-/*#[deriving(Show)]
-pub struct BitMap {
-    t_bits: &[u8],
 }*/
 
-/*impl<T> HeapTupleHeaderData<T> {
-    #[inline]
-    fn t_bits_len(&self) -> uint {
-        self.len
-    }
-
-    pub fn t_bits(&self) -> BitMap {
-        let len =
-        unsafe { ::std::mem::transmute(self.t_bits_.repr().data
-    }
-}*/
-
-pub struct HeapTupleHeader<T, U> {
-    t_len: u32,
-    t_header_: T,
-    t_data: U,
+#[repr(C)]
+pub struct HeapTupleHeader<T, Sized? U> {
+    t_len: u32, // length of *t_data
+    header_: T,
+    t_data: U, // -> tuple header and data
 }
 
 pub struct HeapTupleTemp;
@@ -116,92 +85,41 @@ pub struct HeapTupleDisk {
 
 impl<T, U> Deref<T> for HeapTupleHeader<T, U> {
     fn deref(&self) -> &T {
-        &self.t_header_
+        &self.header_
     }
 }
 
 impl<T, U> DerefMut<T> for HeapTupleHeader<T, U> {
     fn deref_mut(&mut self) -> &mut T {
-        &mut self.t_header_
+        &mut self.header_
     }
 }
 
-pub type HeapTupleDirectData<'a, T, U = NormalTupleHeaderData> = HeapTupleHeader<T, &'a HeapTupleHeaderData<U>>;
+pub type HeapTupleIndirectData<'a, T, U = NormalTupleHeaderData, D = [Datum]> = HeapTupleHeader<T, &'a HeapTupleHeaderData<U, D>>;
 
-pub type HeapTupleContiguousData<T, U = NormalTupleHeaderData> = HeapTupleHeader<T, HeapTupleHeaderData<U>>;
-
-/*#[repr(C,packed)]
-pub struct HeapTupleHeaderInfo<Sized? T> {
-    t_infomask2: u16, // number of attributes + various flags
-    t_infomask: u16, // various flag bits, see below
-    t_hoff: u8, // sizeof header incl. bitmap, padding
-    // ^ - 5 bytes - ^
-    t_bits: T // bitmap of NULLs -- VARIABLE LENGTH
-}
-
-#[repr(C)]
-pub struct HeapTupleHeaderData<Sized? T> {
-    t_heap: HeapTupleFields,
-    t_ctid: ItemPointerData, // current t_ctid of this or newer tuple
-    // ^ - 18 bytes - ^
-    // Fields below here must match MinimalTupleData!
-    t_info: HeapTupleHeaderInfo<T>,
-}
-
-impl Deref<HeapTupleHeaderInfo<[u8]>> for HeapTupleHeaderData<[u8]> {
-    fn deref(&self) -> &HeapTupleHeaderInfo<[u8]> {
-        &self.t_info
-    }
-}
-
-/*impl DerefMut<HeapTupleHeaderInfo> for HeapTupleHeaderData {
-    pub fn deref_mut(&mut self) -> &mut HeapTupleHeaderInfo {
-        &mut self.t_info
-    }
-}*/
-
-/*impl Repr<HeapTupleHeaderData> for [T] {
-}*/*/
+pub type HeapTupleContiguousData<T, U = NormalTupleHeaderData, D = [Datum]> = HeapTupleHeader<T, HeapTupleHeaderData<U, D>>;
 
 pub const MAXIMUM_ALIGNOF: uint = 8;
 
-macro_rules! offset_of(($ty:ty,$field:ident) => {
+macro_rules! with_offset(($ty:ty,$field:ident,$data:ident,$b:expr) => {
     unsafe {
-        //use std::raw::Repr;
-        //use std::ptr::RawPtr;
-        //let data = ::std::mem::transmute::<[u8], $ty>(::std::mem::uninitialized());
-        //let data = ::std::mem::uninitialized::<$ty>();
-        let data = ::std::mem::uninitialized::<$ty>();
-        //let data = 0 as *const $ty;
-
-        //let data = ::std::mem::zeroed::<*const $ty>();
-        //let offset = (&(*data).$field as *const _).to_uint();
-        //let offset = (&(*data).$field as *const _) as uint;//.to_uint();
-        //mem::forget(data);
-        //let offset = (&(*data).$field as *const _ as uint) - (&data as *const _ as uint);//.to_uint();
-        let offset = (&data.$field as *const _ as uint) - (&data as *const _ as uint);//.to_uint();
-        mem::forget(data);
-        offset
+        let $data = 0 as *const $ty;
+        let $field = &(*$data).$field;
+        let result = $b;
+        result
     }
+})
+
+macro_rules! offset_of(($ty:ty,$field:ident) => {
+    with_offset!($ty, $field, data, ($field as *const _ as uint) - (data as uint))
+})
+
+macro_rules! min_align_of_offset(($ty:ty,$field:ident) => {
+    with_offset!($ty, $field, data, ::std::mem::min_align_of_val($field))
 })
 
 // (offsetof(HeapTupleHeaderData, t_infomask2) - sizeof(uint32)) % MAXIMUM_ALIGNOF
 pub const MINIMAL_TUPLE_PADDING: uint = 6;
-
-/*#[repr(C)]
-pub struct MinimalTupleData<Sized? T> {
-    t_len: u32, // actual length of minimal tuple
-    mt_padding: [u8, .. MINIMAL_TUPLE_PADDING],
-    // ^ - 18 bytes - ^
-    // Fields below here must match HeapTupleHeaderData!
-    t_info: HeapTupleHeaderInfo<T>,
-}
-
-impl Deref<HeapTupleHeaderInfo<[u8]>> for MinimalTupleData<[u8]> {
-    fn deref(&self) -> &HeapTupleHeaderInfo<[u8]> {
-        &self.t_info
-    }
-}
 
 /*pub struct Version {
     version: uint,
@@ -228,22 +146,19 @@ struct Database {
 pub struct TransactionManager {
     transactions: *T,
     database: Database,
-}*/*/
+}*/
 
 #[cfg(test)]
 mod tests {
     use std::mem;
-    //use std::raw::Repr;
     use std::u32;
     use super::{
+        Datum,
         HeapTupleContiguousData,
-        HeapTupleDirectData,
+        HeapTupleIndirectData,
         HeapTupleDisk,
-        //HeapTupleHeader,
         HeapTupleHeaderData,
-        //HeapTupleHeaderInfo,
         HeapTupleTemp,
-        //MinimalTupleData,
         MinimalTupleHeaderData,
         NormalTupleHeaderData,
     };
@@ -254,82 +169,55 @@ mod tests {
     }
 
     #[test]
+    fn heap_form_tuple() {
+        let tuple: &HeapTupleHeaderData<MinimalTupleHeaderData, [Datum]> = &HeapTupleHeaderData {
+            data_: MinimalTupleHeaderData,
+            // ^ - 18 bytes (normally) - ^
+            t_infomask2: 0,
+            t_infomask: 0,
+            t_hoff: 0,
+            bits_: [],
+            rest_: [1],
+        };
+        println!("{}", &tuple.rest_);
+        //assert_eq!(mem::align_of_val(&tuple.rest_), super::MAXIMUM_ALIGNOF);
+    }
+
+    #[test]
     // (offsetof(HeapTupleHeaderData, t_infomask2) - sizeof(uint32)) % MAXIMUM_ALIGNOF
     fn minimal_tuple_padding() {
-        /*let offset = unsafe {
-            let data = HeapTupleHeaderData {
-                t_heap: mem::uninitialized(),
-                t_ctid: mem::uninitialized(),
-                t_info: HeapTupleHeaderInfo {
-                    t_infomask2: mem::uninitialized(),
-                    t_infomask: mem::uninitialized(),
-                    t_hoff: mem::uninitialized(),
-                    t_bits: [0u8, .. 0],
-                }
-            };
-            let HeapTupleHeaderData {
-                t_heap: t_heap,
-                t_ctid: t_ctid,
-                t_info: /*HeapTupleHeaderInfo {
-                    t_infomask2: t_infomask2,
-                    t_infomask: t_infomask,
-                    t_hoff: t_hoff,
-                    t_bits: ref t_bits,
-                }*/ref t_info
-            } = data;
-            /*let HeapTupleHeaderInfo {
-                t_infomask2: t_infomask2,
-                t_infomask: t_infomask,
-                t_hoff: t_hoff,
-                t_bits: ref t_bits,
-            } = t_info;*/
-            //let t_bits: &[u8] = &*t_bits;
-            /*let t_info: &HeapTupleHeaderInfo<[u8]> = &HeapTupleHeaderInfo {
-                t_infomask2: t_infomask2,
-                t_infomask: t_infomask,
-                t_hoff: t_hoff,
-                t_bits: *t_bits,
-            };*/
+        type C<T, U, D> = HeapTupleContiguousData<T, U, D>;
+        type I<'a, T, U, D> = HeapTupleIndirectData<'a, T, U, D>;
+        type T = HeapTupleTemp;
+        type S = HeapTupleDisk;
+        type M = MinimalTupleHeaderData;
+        type N = NormalTupleHeaderData;
+        type D = [Datum, .. 0];
+        let offset = offset_of!(HeapTupleHeaderData<M, D>, rest_);
+        let align = min_align_of_offset!(HeapTupleHeaderData<M, D>, rest_);
+        println!("{} {} {}", offset, mem::size_of::<HeapTupleHeaderData<M, D>>(), align);
+        assert_eq!(align, super::MAXIMUM_ALIGNOF);
+        let offset = offset_of!(HeapTupleHeaderData<N, D>, rest_);
+        let align = min_align_of_offset!(HeapTupleHeaderData<N, D>, rest_);
+        println!("{} {} {}", offset, mem::size_of::<HeapTupleHeaderData<N, D>>(), align);
+        assert_eq!(align, super::MAXIMUM_ALIGNOF);
 
-            let data: &HeapTupleHeaderData<[u8]> = &HeapTupleHeaderData {
-                t_heap: t_heap,
-                t_ctid: t_ctid,
-                t_info: *t_info,
-            };
-            println!("{}", data.t_bits.len());
-            //let data = mem::transmute::<_, &HeapTupleHeaderData<[u8]>>(&data);
-            //let offset = (&(*data).$field as *const _).to_uint();
-            //let offset = (&data.t_info as *const _ as uint) - (data as *const _ as uint);
-            //let offset = (&data.t_info as *const _ as uint) - (&data as *const _ as uint);
-            //mem::forget(data);
-            //offset
-            0
-        };*/
+        let offset = offset_of!(C<T, M, D>, t_data);
+        println!("{} {}", offset, mem::size_of::<C<T, M, D>>());
+        let offset = offset_of!(I<T, N, D>, t_data);
+        println!("{} {}", offset, mem::size_of::<I<T, N, D>>());
+        let offset = offset_of!(I<S, N, D>, t_data);
+        println!("{} {}", offset, mem::size_of::<I<S, N, D>>());
+        let offset = offset_of!(C<S, M, D>, t_data);
+        println!("{} {}", offset, mem::size_of::<C<S, M, D>>());
+        let offset = offset_of!(C<T, N, D>, t_data);
+        println!("{} {}", offset, mem::size_of::<C<T, N, D>>());
+        let offset = offset_of!(C<S, N, D>, t_data);
+        println!("{} {}", offset, mem::size_of::<C<S, N, D>>());
 
-        //let offset = offset_of!(HeapTupleHeaderData<MinimalTupleHeaderData>, t_infomask2);
-        //println!("{} {}", offset, mem::size_of::<HeapTupleHeaderData<MinimalTupleHeaderData>>());
-        let offset = offset_of!(HeapTupleContiguousData<HeapTupleTemp, MinimalTupleHeaderData>, t_data);
-        println!("{} {}", offset, mem::size_of::<HeapTupleContiguousData<HeapTupleTemp, MinimalTupleHeaderData>>());
-        let offset = offset_of!(HeapTupleDirectData<HeapTupleTemp>, t_data);
-        println!("{} {}", offset, mem::size_of::<HeapTupleDirectData<HeapTupleTemp>>());
-        let offset = offset_of!(HeapTupleDirectData<HeapTupleDisk>, t_data);
-        println!("{} {}", offset, mem::size_of::<HeapTupleDirectData<HeapTupleDisk>>());
-        let offset = offset_of!(HeapTupleContiguousData<HeapTupleDisk, MinimalTupleHeaderData>, t_data);
-        println!("{} {}", offset, mem::size_of::<HeapTupleContiguousData<HeapTupleDisk, MinimalTupleHeaderData>>());
-        let offset = offset_of!(HeapTupleContiguousData<HeapTupleTemp>, t_data);
-        println!("{} {}", offset, mem::size_of::<HeapTupleContiguousData<HeapTupleTemp>>());
-        let offset = offset_of!(HeapTupleContiguousData<HeapTupleDisk>, t_data);
-        println!("{} {}", offset, mem::size_of::<HeapTupleContiguousData<HeapTupleDisk>>());
-
-        let offset = offset_of!(HeapTupleHeaderData<NormalTupleHeaderData>, t_infomask2);
-        //println!("{} {}", offset, mem::size_of::<HeapTupleHeaderData<NormalTupleHeaderData>>());
+        let offset = offset_of!(HeapTupleHeaderData<N, D>, t_infomask2);
+        //println!("{} {}", offset, mem::size_of::<HeapTupleHeaderData<N, D>>());
         let minimal_tuple_padding = (offset - u32::BYTES) % super::MAXIMUM_ALIGNOF;
         assert_eq!(minimal_tuple_padding, super::MINIMAL_TUPLE_PADDING);
-
-        /*let offset = offset_of!(HeapTupleHeaderData<[u8, ..0]>, t_info);
-        let minimal_tuple_padding = (offset - u32::BYTES) % super::MAXIMUM_ALIGNOF;
-        assert_eq!(minimal_tuple_padding, super::MINIMAL_TUPLE_PADDING);
-        let offset = offset_of!(MinimalTupleData<[u8, ..0]>, t_info);
-        assert_eq!(minimal_tuple_padding, super::MINIMAL_TUPLE_PADDING);*/
     }
 }
